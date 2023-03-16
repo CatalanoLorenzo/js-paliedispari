2dc7625595f50f3b305171091ed78d7f1706a7ee
CatalanoLorenzo <lorenzocatalano1995@outlook.it>
2023-03-16 15:11:44 +0100

    add function for clear code

--- asset/js/main.js
+++ asset/js/main.js
@@ -1,7 +1,8 @@
 
 /* Palidroma
 Chiedere all’utente di inserire una parola Creare una funzione per capire se la parola inserita è palindroma  */
-/* function pali(user_word) {
+//-------------MAKE FUNCTION----------------------
+function pali(user_word) {
     //scomporre la strinca in un arrei
     const user_word_split = user_word.split('')
     console.log(user_word_split)
@@ -18,7 +19,26 @@ Chiedere all’utente di inserire una parola Creare una funzione per capire se l
         return result = false;
     }
 }
+function pari_o_dispari(chois_user) {
+    if (chois_user == 'pari') {
+        return valid_chois = true
+    } else if (chois_user == 'dispari') {
+        return valid_chois = false
+    } else {
+        alert('scelta non valida ,controlla ciò che hai scritto!!!!!')
+        chois_user = prompt('Pari o Dispari?').toLowerCase()
+    }
+}
+function valid_chois_number_1_and_5 (number_user){
+    if (number_user <= 5 && number_user >= 1) {
+
+    } else {
+        alert('Il numero che hai scelto non è compreso tra 1 e 5 o non è un numero !!!!!!!!!!!!!')
+        number_user = (Number(prompt('Scegli un numero da 1 a 5')))
+    }
+}
 //-------------END MAKE FUNCTION------------------
+/* 
 let next = true
 //inserimento parola da parte dell'utente
 while (next) {
@@ -32,7 +52,7 @@ if (result) {
 }
 next = confirm('vuoi continuare?')
 }
-//-------------MAKE FUNCTION----------------------
+
  */
 /*Pari e Dispari
 L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
@@ -42,42 +62,29 @@ L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
 //l'utente sceglie pario o dispari
 let chois_user = prompt('Pari o Dispari?').toLowerCase()
 console.log(chois_user)
-let valid_chois
-if(chois_user == 'pari'){
-    valid_chois = true
-}else if(chois_user == 'dispari'){
-    valid_chois = false
-}else{
-    alert('scelta non valida ,controlla ciò che hai scritto!!!!!')
-    chois_user = prompt('Pari o Dispari?').toLowerCase()
-}
+pari_o_dispari(chois_user)
+
 //l'utente sceglie un numero
 const number_user = (Number(prompt('Scegli un numero da 1 a 5')))
 console.log(number_user)
-if(number_user<= 5 && number_user >= 1){
-
-
-}else{
-    alert('Il numero che hai scelto non è compreso tra 1 e 5 o non è un numero !!!!!!!!!!!!!')
-    number_user = (Number(prompt('Scegli un numero da 1 a 5')))
-}
+valid_chois_number_1_and_5 (number_user)
 
 //la CPU sceglie un numero random
-const number_cpu = Math.round((Math.random() * 4 )+ 1)
+const number_cpu = Math.round((Math.random() * 4) + 1)
 console.log(number_cpu)
 // Somma del numero utente con quello della CPU
 const total_number = number_cpu + number_user
 //verificare se il risultato è pari o dispari 
 let pari
-if(total_number % 2 == 0){
+if (total_number % 2 == 0) {
     pari = true
-}else{
+} else {
     pari = false
 }
- 
+
 //confrontare con la scelta dell'utente
-if(valid_chois = pari){
+if (pari_o_dispari = pari) {
     console.log(' Baravo hai vinto ')
-}else{
+} else {
     console.log('Hai perso ');
 }
