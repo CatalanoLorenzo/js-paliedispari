ef2236486581ca661642fff74cf0126923cc6de7
CatalanoLorenzo <lorenzocatalano1995@outlook.it>
2023-03-16 13:47:30 +0100

    add const user_word and split

--- asset/js/main.js
+++ asset/js/main.js
@@ -1,10 +1,12 @@
+
 /* Palidroma
-Chiedere all’utente di inserire una parola Creare una funzione per capire se la parola inserita è palindroma
+Chiedere all’utente di inserire una parola Creare una funzione per capire se la parola inserita è palindroma  */
 
 //inserimento parola da parte dell'utente
-
+const user_word = prompt('scegli una parola !')
 //scomporre la strinca in un arrei
-
+const user_word_split = user_word.split('')
+console.log(user_word_split)
 //invertire l'orndine di lettura dell'array
 
 //creare una nuova parrola con l'array invertito 
@@ -13,5 +15,8 @@ Chiedere all’utente di inserire una parola Creare una funzione per capire se l
 
 
 
-Pari e Dispari
-L’utente sceglie pari o dispari e inserisce un numero da 1 a 5. Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione). Sommiamo i due numeri Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione) Dichiariamo chi ha vinto. */
+/*Pari e Dispari
+L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
+ Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
+  Sommiamo i due numeri Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione) Dichiariamo chi ha vinto.
+ */
